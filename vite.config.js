a8cf02486df63a62befbb9ea7b9afefc55cc9dec
Madhusha Weerasiri <mdweerasiri.com>
2025-11-04 18:11:32 +0530

    vite.config.js - proxy set up

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/tmf-api': {
+        target: 'https://markethub-api-gateway.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path,
+      },
+    },
+  },
 })
