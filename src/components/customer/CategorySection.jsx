023eb5a11f0a33cf4c2969cadb8e8539868787d9
Madhusha Weerasiri <mdweerasiri.com>
2025-11-06 08:59:19 +0530

    added a console.log for debuging in CategorySection

--- src/components/customer/CategorySection.jsx
+++ src/components/customer/CategorySection.jsx
@@ -10,6 +10,7 @@ export default function CategorySection() {
     useEffect(() => {
         async function fetchCategory() {
             try {
+                console.log("Fetching categories from:", VITE_ENDPOINT_TMF620_CATEGORY);
                 const res = await fetch(`${VITE_ENDPOINT_TMF620_CATEGORY}`);
                 const data = await res.json();
 
